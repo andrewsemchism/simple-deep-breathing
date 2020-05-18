af9d75c46f3c92afe51ebf6680651e13386c5aac
Andrew Semchism <andrew0187@gmail.com>
2020-05-18 00:54:42 -0400

    Add settings menu

--- breath.js
+++ breath.js
@@ -1,12 +1,11 @@
 //selects the breathing-circle class
 const Breathingcircle = document.querySelector('.breathing-circle');
 
-let breathInTime = 3000
-let breathOutTime = 4000
+let breathInTime = 1000
+let breathOutTime = 1000
 
 //breathing-animation class
 function breathAnimation() {
-  console.log('Breath in time: ' + breathInTime)
   Breathingcircle.animate([
     //Keyframes
     { transform: 'scale(0.3)' },
@@ -30,6 +29,55 @@ function breathAnimation() {
   });
 }
 
+breathAnimation(breathInTime, breathOutTime)
+let interval = setInterval(breathAnimation, breathInTime + breathOutTime)
 
-breathAnimation()
-setInterval(breathAnimation, breathInTime + breathOutTime)
+// selects the range inputs and the number inputs
+const breathInRangeInput = document.getElementById('breath-in-speed-range')
+const breathInNumberInput = document.getElementById('breath-in-speed-number')
+const breathOutRangeInput = document.getElementById('breath-out-speed-range')
+const breathOutNumberInput = document.getElementById('breath-out-speed-number')
+
+
+// controls for the breathing animation
+breathInNumberInput.addEventListener('input', function (e) {
+  breathInRangeInput.value = e.target.value;
+  breathInTime = breathInNumberInput.value * 1000
+  console.log(breathInTime + breathOutTime)
+  clearInterval(interval);
+  breathAnimation(breathInTime + breathOutTime)
+  interval = setInterval(breathAnimation, breathInTime + breathOutTime)
+});
+breathInRangeInput.addEventListener('input', function (e) {
+  breathInNumberInput.value = e.target.value;
+  breathInTime = breathInNumberInput.value * 1000
+  console.log(breathInTime + breathOutTime)
+  clearInterval(interval);
+  breathAnimation(breathInTime + breathOutTime)
+  interval = setInterval(breathAnimation, breathInTime + breathOutTime)
+});
+
+breathOutNumberInput.addEventListener('input', function (e) {
+  breathOutRangeInput.value = e.target.value;
+  breathOutTime = breathOutNumberInput.value * 1000
+  console.log(breathInTime + breathOutTime)
+  clearInterval(interval);
+  breathAnimation(breathInTime + breathOutTime)
+  interval = setInterval(breathAnimation, breathInTime + breathOutTime)
+});
+breathOutRangeInput.addEventListener('input', function (e) {
+  breathOutNumberInput.value = e.target.value;
+  breathOutTime = breathOutNumberInput.value * 1000
+  console.log(breathInTime + breathOutTime)
+  clearInterval(interval);
+  breathAnimation(breathInTime + breathOutTime)
+  interval = setInterval(breathAnimation, breathInTime + breathOutTime)
+});
+
+
+//navbar
+const settings = document.querySelector('.sidebar')
+function toggleNav() {
+  settings.classList.toggle("show-side-bar");
+  //settings.style.transform = "translate3d(0vw, 0, 0)"
+}
